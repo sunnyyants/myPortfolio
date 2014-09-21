a32213048767842b821474487a6b85f1dd221f3f
Tingshen Yan <sunnyyants@gmail.com>
2014-09-21 01:33:49 -0700

    refactor codes using Angular.js

--- bootstrap/js/portfolio.js
+++ bootstrap/js/portfolio.js
@@ -36,7 +36,7 @@
                 '<a ng-href="{{socalMedia.href}}" ng-attr-id="{{socalMedia.id}}"><img ng-src="{{socalMedia.src}}" ng-class="center-block"/></a>' +
                 '</div>'
         }
-    })
+    });
 
     app.controller('SkillsetController',['$scope',function($scope){
         $scope.skills = [{
@@ -55,7 +55,7 @@
             height:300,
             title:'Javascripts'
         }]
-    }])
+    }]);
 
     app.directive("mySkillSet", function(){
         return{
@@ -64,7 +64,7 @@
                 '<canvas ng-attr-id="{{skill.id}}" width="{{skill.width}}" height="{{skill.height}}" class="center-block"></canvas>' +
                 '<h3 class="text-center">{{skill.title}}</h3></div>'
         }
-    })
+    });
 
     app.controller('myInterestsController',['$scope',function($scope){
         $scope.legends = [
@@ -132,6 +132,92 @@
             template:'<div class="col-md-4" ng-repeat="info in interestsInfos">' +
                 '<i class="{{info.icon}}"></i>' +
                 '<p>{{info.text}}</p></div>'
+        }
+    });
+
+    app.controller('aboutMeController',['$scope',function($scope){
+        $scope.images = [
+            {
+                id:0,
+                imgClass:'item active',
+                buttonClass:'active',
+                src:'bootstrap/images/IMG_20130615_180218.jpg'
+            },{
+                id:1,
+                imgClass:'item',
+                buttonClass:'',
+                src:'bootstrap/images/Screenshot_2014-09-21-00-58-52.png'
+            },{
+                id:2,
+                imgClass:'item',
+                buttonClass:'',
+                src:'bootstrap/images/IMG_20140308_105224.jpg'
+            },{
+                id:3,
+                imgClass:'item',
+                buttonClass:'',
+                src:'bootstrap/images/IMG_20140827_173253.jpg'
+            },{
+                id:4,
+                imgClass:'item',
+                buttonClass:'',
+                src:'bootstrap/images/IMG_20140614_223157.jpg'
+            }
+        ];
+
+        $scope.introductions = [
+            "Hi there, my name is Tingshen Yan, I am a fresh Master of Science in Electrical Computer Engineering from Northeastern University, Boston, Massachusetts.",
+            "Currently I am working as a web developer volunteer at the Sankofa Center, which is one of the non-profit organizations in Silicon Vally. My responsibilities are designing and building an interactive retail partners' distribution map in their current site.",
+            "I am very interested in web development and I am actively seeking a full-time position in web development area."
+        ]
+    }]);
+
+    app.directive('myGallery',function(){
+        return{
+            restrict:'E',
+            template:'<ol class="carousel-indicators">' +
+                '<li data-target="#carousel-example-generic" ng-repeat="image in images" data-slide-to="{{image.id}}" class="{{image.buttonClass}}"></li></ol>' +
+                '<div class="carousel-inner">' +
+                '<div ng-repeat="image in images" class="{{image.imgClass}}">' +
+                '<img ng-src="{{image.src}}" class="img-circle img-responsive center-block"/></div></div>  '
+        }
+    });
+
+    app.directive('myInfos',function(){
+        return{
+            restrict:'E',
+            template:'<div class="col-md-4" ng-repeat="info in introductions"><p>{{info}}</p></div>'
+        }
+    });
+
+
+    app.controller('myProjectController',['$scope',function($scope){
+        $scope.projects = [{
+            pid:'pj1',
+            pjImgId:'pjp1',
+            header:'Delicious',
+            description:'Designed and implemented a restaurant online ordering application using MySQL, Hibernate, Struts2, JSPs and Twitter Bootstrap.'
+        },{
+            pid:'pj2',
+            pjImgId:'pjp2',
+            header:'Multi-Label Problems',
+            description:'Investigated the the Ada-boost algorithm and built a classifier using Matlab. Tried to solve the multi-label classification problems using the Ada-boost classifier and analysed its quality.'
+        },{
+            pid:'pj3',
+            pjImgId:'pjp3',
+            header:'Blackjack',
+            description:'Implemented a Python blackjack game. Used the Pygame framework and MySQL database.'
+        }
+        ]
+    }]);
+
+    app.directive('myProjects',function(){
+        return{
+            restrict:'E',
+            template:'<div class="col-md-10 col-md-offset-1" ng-repeat="project in projects" ng-attr-id="{{project.pid}}">' +
+                '<div class="col-md-4 img-circle center-block" ng-attr-id="{{project.pjImgId}}"></div>' +
+                '<div class="col-md-9"><h2 class="page-header">{{project.header}}!</h2>' +
+                '<p>{{project.description}}</p></div></div>'
         }
     })
 })();
